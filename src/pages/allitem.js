309ea14ee549e7680b06493b5df715d0b863a1d5
Choakthawee Nikhomkhai <choakthawee.n@ku.th>
2023-10-23 17:00:11 +0700

    อัพเดตหน้าเว็บหลัก

--- src/pages/allitem.js
+++ src/pages/allitem.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './allitem.css'
-import valorant from '../images/valorant.jpg'
-import roblox from '../images/roblox.jpg'
-import rov from '../images/rov.jpg'
-import apex from '../images/apex.jpg'
+import valorant from '../images/valorant.png'
+import roblox from '../images/roblox.png'
+import rov from '../images/rov.png'
+import apex from '../images/apex.png'
 
 const AllItem = () => {
   return (
@@ -12,29 +12,33 @@ const AllItem = () => {
         <div style={{display:'flex',justifyContent:'center',alignItems:'center'}}>
           <h1 className='font-allitem'>ร้านค้า</h1>
         </div>
+
+        <div style={{display:'flex',justifyContent:'center',alignItems:'center'}}>
+          <h1 className='font-suballitem'>เกมทั้งหมด</h1>
+        </div>
         
         <div className='my-container'> 
           <div className='boxallflex1'>
             <div className='boxallitem'>
               <img src={valorant } alt="valorant " className='valorant' />
-              <p>valorant</p>
+              <p style={{paddingLeft:'1rem',paddingTop:'0.3rem',fontWeight:'bold',backgroundColor:'#ff6900',width:'100%',height:'100%',color:'white',fontSize:'24px'}}>บริการเติมเกม Valorant</p>
             </div>
 
             <div className='boxallitem'>
               <img src={roblox } alt="roblox " className='valorant' />
-              <p>roblox</p>
+              <p style={{paddingLeft:'1rem',paddingTop:'0.3rem',fontWeight:'bold',backgroundColor:'#ff6900',width:'100%',height:'100%',color:'white',fontSize:'24px'}}>บริการเติมเกม Roblox</p>
             </div>
           </div>
 
           <div className='boxallflex1'>
             <div className='boxallitem'>
               <img src={rov } alt="rov " className='valorant' />
-              <p>rov</p>
+              <p style={{paddingLeft:'1rem',paddingTop:'0.3rem',fontWeight:'bold',backgroundColor:'#ff6900',width:'100%',height:'100%',color:'white',fontSize:'24px'}}>บริการเติมเกม ROV</p>
             </div>
 
             <div className='boxallitem'>
               <img src={apex } alt="apex " className='valorant' />
-              <p>apex</p>
+              <p style={{paddingLeft:'1rem',paddingTop:'0.3rem',fontWeight:'bold',backgroundColor:'#ff6900',width:'100%',height:'100%',color:'white',fontSize:'24px'}}>บริการเติมเกม Apex Legend (PC)</p>
             </div>
           </div>
         </div>
