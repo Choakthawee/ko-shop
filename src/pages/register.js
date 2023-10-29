f63a99d6f0893acc7dc30d2ade6d7b6486e79b03
OATKASIDIT <revoitz158@gmail.com>
2023-10-30 00:35:12 +0700

    เสร็จregsiterแล้ว

--- src/pages/register.js
+++ src/pages/register.js
@@ -10,13 +10,13 @@ import videoBg from '../images/Halloween.mp4';
 import Swal from 'sweetalert2';
 
 const Register = () => {
-  const [username,setUsername] = useState("");
-  const [password,setPassword] = useState("");
-  const [confirmpassword,setConfirmpassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmpassword, setConfirmpassword] = useState("");
   // const [money,setMoney] = useState(0);
-  const [email,setEmail] = useState("");
-  const [userList,setUser]=useState([]);
-  
+  const [email, setEmail] = useState("");
+  const [userList, setUser] = useState([]);
+
   const history = useHistory();
 
   // //ดึงข้อมูลผู้ใช้ทั้งหมด
@@ -26,7 +26,7 @@ const Register = () => {
   //   });
   // }
 
-  const addUsername = () =>{
+  const addUsername = () => {
     if (!username || !password || !confirmpassword || !email) {
       // กรอกข้อมูลไม่ครบ ใช้ SweetAlert2 เพื่อแสดงข้อความแจ้งเตือน
       Swal.fire({
@@ -36,7 +36,7 @@ const Register = () => {
       });
       return;
     }
-  
+
     if (password.length < 8 || password.length > 20) {
       // ตรวจสอบความยาวของรหัสผ่าน
       Swal.fire({
@@ -46,7 +46,7 @@ const Register = () => {
       });
       return;
     }
-  
+
     if (password !== confirmpassword) {
       // ตรวจสอบว่ารหัสผ่านและยืนยันรหัสผ่านตรงกันหรือไม่
       Swal.fire({
@@ -56,7 +56,7 @@ const Register = () => {
       });
       return;
     }
-  
+
     if (!email.includes('@')) {
       // ตรวจสอบว่าอีเมลมี @ หรือไม่
       Swal.fire({
@@ -66,36 +66,33 @@ const Register = () => {
       });
       return;
     }
-    axios.post('http://localhost:3001/create',{
+    axios.post('http://localhost:3001/create', {
       username: username,
       password: password,
       confirmpassword: confirmpassword,
-      email:email
+      email: email
     }).then((response) => {
       setUser({
-        ...userList,    
+        ...userList,
         username: username,
         password: password,
         confirmpassword: confirmpassword,
-        email:email
+        email: email
       })
+      Swal.fire({
+        icon: 'success',
+        title: 'สมัครสมาชิกสำเร็จ',
+        text: 'ยินดีต้อนรับ! คุณได้สมัครสมาชิกแล้ว'
+      });
       history.push('/login');
     })
-    .catch((error) => {
-      if (error.response) {
+      .catch((error) => {
         Swal.fire({
           icon: 'error',
-          title: 'Error',
-          text: error.response.data // Display the error response message
+          title: 'สมัครสมาชิกล้มเหลว',
+          text: 'กรุณาสมัครสมาชิกอีกรอบ'
         });
-      } else {
-        Swal.fire({
-          icon: 'error',
-          title: 'Error',
-          text: 'Something went wrong' // Display a generic error message
-        });
-      }
-    });
+      });
   }
 
   return (
@@ -103,59 +100,59 @@ const Register = () => {
       <video autoPlay loop muted className='video-bg'>
         <source src={videoBg} type='video/mp4' />
       </video>
-      <div className='registerbg' data-aos = "fade-up" data-aos-delay = "0">
+      <div className='registerbg' data-aos="fade-up" data-aos-delay="0">
         <h1 className='fontRegister'>สมัครสมาชิก</h1>
         <div className="line-3"></div>
-        
-        <div style={{display:'flex',justifyContent:'center',alignItems:'center',marginTop:'50px'}}>
+
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '50px' }}>
           <Form>
-            <Form.Group style={{marginBottom:'10px'}}>
+            <Form.Group style={{ marginBottom: '10px' }}>
               <Form.Label>Username</Form.Label>
               <Form.Control
                 type='text'
                 name='username'
                 placeholder='กรุณากรอก username'
-                style={{borderRadius:'15px',borderWidth:'1px'}}
-                onChange={(event) =>{
+                style={{ borderRadius: '15px', borderWidth: '1px' }}
+                onChange={(event) => {
                   setUsername(event.target.value)
                 }}
               />
             </Form.Group>
 
-            <Form.Group style={{marginBottom:'10px'}}>
+            <Form.Group style={{ marginBottom: '10px' }}>
               <Form.Label>Password</Form.Label>
               <Form.Control
                 type='password'
                 name='password'
                 placeholder='กรุณากรอก password'
-                style={{borderRadius:'15px',borderWidth:'1px'}}
-                onChange={(event) =>{
+                style={{ borderRadius: '15px', borderWidth: '1px' }}
+                onChange={(event) => {
                   setPassword(event.target.value)
                 }}
               />
             </Form.Group>
 
-            <Form.Group style={{marginBottom:'10px'}}>
+            <Form.Group style={{ marginBottom: '10px' }}>
               <Form.Label>Confirm Password</Form.Label>
               <Form.Control
                 type='password'
                 name='confirmPassword'
                 placeholder='กรุณากรอก confirm password'
-                style={{borderRadius:'15px',borderWidth:'1px'}}
-                onChange={(event) =>{
+                style={{ borderRadius: '15px', borderWidth: '1px' }}
+                onChange={(event) => {
                   setConfirmpassword(event.target.value)
                 }}
               />
             </Form.Group>
 
-            <Form.Group style={{marginBottom:'10px'}}>
+            <Form.Group style={{ marginBottom: '10px' }}>
               <Form.Label>Email</Form.Label>
               <Form.Control
                 type='email'
                 name='email'
                 placeholder='กรุณากรอก email'
-                style={{borderRadius:'15px',borderWidth:'1px'}}
-                onChange={(event) =>{
+                style={{ borderRadius: '15px', borderWidth: '1px' }}
+                onChange={(event) => {
                   setEmail(event.target.value)
                 }}
               />
@@ -163,18 +160,18 @@ const Register = () => {
           </Form>
         </div>
 
-          <div style={{ display: 'flex', justifyContent: 'center', marginTop: '2%' }}>
-            <Button className= "button" type="button" onClick={addUsername} style={{ backgroundColor: '#06D6A0', color: 'white', borderRadius: '10px', width: '300px', height: '50px', fontSize: '20px', marginRight: '10%', borderWidth: '0' }}>
-              <FontAwesomeIcon icon={faUserPlus} style={{ marginRight: 5 }} /> สมัครสมาชิก
+        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '2%' }}>
+          <Button className="button" type="button" onClick={addUsername} style={{ backgroundColor: '#06D6A0', color: 'white', borderRadius: '10px', width: '300px', height: '50px', fontSize: '20px', marginRight: '10%', borderWidth: '0' }}>
+            <FontAwesomeIcon icon={faUserPlus} style={{ marginRight: 5 }} /> สมัครสมาชิก
+          </Button>
+
+          <Link to="/allitem">
+            <Button className="button" type="button" style={{ backgroundColor: '#E63946', color: 'white', borderRadius: '10px', width: '300px', height: '50px', fontSize: '20px', marginBottom: '2%', borderWidth: '0' }}>
+              <FontAwesomeIcon icon={faBackward} style={{ marginRight: 5 }} /> กลับสู่หน้าหลัก
             </Button>
+          </Link>
 
-            <Link to="/allitem">
-              <Button className= "button" type="button" style={{ backgroundColor: '#E63946', color: 'white', borderRadius: '10px', width: '300px', height: '50px', fontSize: '20px', marginBottom: '2%', borderWidth: '0' }}>
-                <FontAwesomeIcon icon={faBackward} style={{ marginRight: 5 }} /> กลับสู่หน้าหลัก
-              </Button>
-            </Link>
-
-          </div>
+        </div>
 
         <div className="line-3"></div>
       </div>
