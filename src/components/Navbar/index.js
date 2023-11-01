abaf23aa6963da8be4a90f2cb2ade4f53f4c4bb8
Choakthawee Nikhomkhai <choakthawee.n@ku.th>
2023-11-01 23:23:13 +0700

    update-navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,71 +1,91 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  Nav,
-  NavLink,
-  Bars,
-  NavMenu,
-} from './NavbarElements';
-import './index.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserPlus, faUser, faCartShopping, faCirclePlus } from '@fortawesome/free-solid-svg-icons';
-import { useLocation } from 'react-router-dom';
+	faUserPlus,
+	faUser,
+	faCartShopping,
+	faCirclePlus,
+} from "@fortawesome/free-solid-svg-icons";
+import { useLocation } from "react-router-dom";
 
+import "./index.css";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
-  const [loggedInUsername, setLoggedInUsername] = useState(localStorage.getItem('username'));
-  const location = useLocation();
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
-  const handleStorageChange = () => {
-    setLoggedInUsername(localStorage.getItem('username'));
-  };
+	const [menuOpen, setMenuOpen] = useState(false);
+	const [loggedInUsername, setLoggedInUsername] = useState(
+		localStorage.getItem("username")
+	);
+	const location = useLocation();
+	const handleStorageChange = () => {
+		setLoggedInUsername(localStorage.getItem("username"));
+	};
 
-  useEffect(() => {
-    window.addEventListener('storage', handleStorageChange);
+	useEffect(() => {
+		window.addEventListener("storage", handleStorageChange);
 
-    return () => {
-      window.removeEventListener('storage', handleStorageChange);
-    };
-  }, []);
+		return () => {
+			window.removeEventListener("storage", handleStorageChange);
+		};
+	}, []);
 
-  useEffect(() => {
-    setLoggedInUsername(localStorage.getItem('username'));
-  }, [location]); 
+	useEffect(() => {
+		setLoggedInUsername(localStorage.getItem("username"));
+	}, [location]);
 
-  return (
-    <>
-      <Nav>
-        <NavLink to='/'>
-          <img src={require('../../images/logo.png')} alt='logo' className='Logo' />
-        </NavLink>
-        <Bars onClick={toggleMenu} />
-        <NavMenu className={menuOpen ? 'active' : ''}>
-          <NavLink to='/allitem' >
-            <FontAwesomeIcon icon={faCartShopping} style={{ marginRight: 5 }} /> หมวดหมู่สินค้าทั้งหมด
-          </NavLink>
-          <NavLink to='/topup' >
-            <FontAwesomeIcon icon={faCirclePlus} style={{ marginRight: 5 }} /> เติมเงิน
-          </NavLink>
-          {loggedInUsername ? (
-            <NavLink to='/profile' >
-              <FontAwesomeIcon icon={faUser} style={{ marginRight: 5 }} /> โปรไฟล์
-            </NavLink>
-          ) : (
-            <>
-              <NavLink to='/register' >
-                <FontAwesomeIcon icon={faUserPlus} style={{ marginRight: 5 }} /> สมัครสมาชิก
-              </NavLink>
-              <NavLink to='/login' >
-                <FontAwesomeIcon icon={faUser} style={{ marginRight: 5 }} /> เข้าสู่ระบบ
-              </NavLink>
-            </>
-          )}
-        </NavMenu>
-      </Nav>
-    </>
-  );
+	return (
+		<nav>
+			<NavLink to="/">
+				<img
+					src={require("../../images/logo.png")}
+					alt="logo"
+					className="Logo"
+				/>
+			</NavLink>
+			<div className="menu" onClick={() => setMenuOpen(!menuOpen)}>
+				<span></span>
+				<span></span>
+				<span></span>
+			</div>
+			<ul className={menuOpen ? "open" : ""}>
+				<li>
+					<NavLink to="/allitem">
+						<FontAwesomeIcon icon={faCartShopping} style={{ marginRight: 5 }} />{" "}
+						หมวดหมู่สินค้าทั้งหมด
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to="/topup">
+						<FontAwesomeIcon icon={faCirclePlus} style={{ marginRight: 5 }} />{" "}
+						เติมเงิน
+					</NavLink>
+				</li>
+				{loggedInUsername ? (
+					<li>
+						<NavLink to="/profile">
+							<FontAwesomeIcon icon={faUser} style={{ marginRight: 5 }} />{" "}
+							โปรไฟล์
+						</NavLink>
+					</li>
+				) : (
+					<>
+						<li>
+							<NavLink to="/register">
+								<FontAwesomeIcon icon={faUserPlus} style={{ marginRight: 5 }} />{" "}
+								สมัครสมาชิก
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to="/login">
+								<FontAwesomeIcon icon={faUser} style={{ marginRight: 5 }} />{" "}
+								เข้าสู่ระบบ
+							</NavLink>
+						</li>
+					</>
+				)}
+			</ul>
+		</nav>
+	);
 };
 
 export default Navbar;
--- src/pages/allitem.js
+++ src/pages/allitem.js
@@ -1,46 +1,73 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './allitem.css';
-import valorant from '../images/valorant.png';
-import roblox from '../images/roblox.png';
-import rov from '../images/rov.png';
-import apex from '../images/apex.png';
-import videoBg from '../images/Halloween.mp4';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./allitem.css";
+import valorant from "../images/valorant.png";
+import roblox from "../images/roblox.png";
+import rov from "../images/rov.png";
+import apex from "../images/apex.png";
+import videoBg from "../images/Halloween.mp4";
 
 const AllItem = () => {
-  return (
-    <div className='allitem'>
-      <div className='allitemScreen'>
-        <video autoPlay loop muted className='video-bg'>
-          <source src={videoBg} type='video/mp4' />
-        </video>
-        <div className='centered-container'>
-          <h1 className='font-allitem' data-aos="fade-up">ร้านค้า</h1>
-        </div>
-        <div className='centered-container'>
-          <h1 className='font-suballitem' data-aos="fade-up" data-aos-delay="50">เกมทั้งหมด</h1>
-        </div>
-        <div className='my-container'>
-          <div className='boxallflex2'>
-            <Link to="/valorant" className='boxallitem' data-aos="fade-up" data-aos-delay="100" data-aos-once="true">
-              <img src={valorant} alt="valorant" className='valorant' />
-            </Link>
-            <Link to="/roblox" className='boxallitem' data-aos="fade-up" data-aos-delay="150" data-aos-once="true">
-              <img src={roblox} alt="roblox" className='valorant' />
-            </Link>
-          </div>
-          <div className='boxallflex2'>
-            <Link to="/rov" className='boxallitem' data-aos="fade-up" data-aos-delay="200" data-aos-once="true">
-              <img src={rov} alt="rov" className='valorant' />
-            </Link>
-            <Link to="/apex" className='boxallitem' data-aos="fade-up" data-aos-delay="250" data-aos-once="true">
-              <img src={apex} alt="apex" className='valorant' />
-            </Link>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="allitem">
+			<div className="allitemScreen">
+				<video autoPlay loop muted className="video-bg">
+					<source src={videoBg} type="video/mp4" />
+				</video>
+				<div className="centered-container">
+					<h1 className="font-allitem" data-aos="fade-up">
+						ร้านค้า
+					</h1>
+				</div>
+				<div className="centered-container">
+					<h1
+						className="font-suballitem"
+						data-aos="fade-up"
+						data-aos-delay="50">
+						เกมทั้งหมด
+					</h1>
+				</div>
+				<div className="my-container">
+					<div className="boxallflex2">
+						<Link
+							to="/valorant"
+							className="boxallitem"
+							data-aos="fade-up"
+							data-aos-delay="100"
+							data-aos-once="true">
+							<img src={valorant} alt="valorant" className="valorant" />
+						</Link>
+						<Link
+							to="/roblox"
+							className="boxallitem"
+							data-aos="fade-up"
+							data-aos-delay="150"
+							data-aos-once="true">
+							<img src={roblox} alt="roblox" className="valorant" />
+						</Link>
+					</div>
+					<div className="boxallflex2">
+						<Link
+							to="/rov"
+							className="boxallitem"
+							data-aos="fade-up"
+							data-aos-delay="200"
+							data-aos-once="true">
+							<img src={rov} alt="rov" className="valorant" />
+						</Link>
+						<Link
+							to="/apex"
+							className="boxallitem"
+							data-aos="fade-up"
+							data-aos-delay="250"
+							data-aos-once="true">
+							<img src={apex} alt="apex" className="valorant" />
+						</Link>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default AllItem;
